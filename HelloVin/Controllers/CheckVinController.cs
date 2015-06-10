b948b1f9e6c2815c1f39b33c772ad984aab4b430
tiedemann <sussi@workport.se>
2015-06-10 12:58:42 +0200

    Added CheckVonController

---
+++ HelloVin/Controllers/CheckVinController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloVin.Controllers
+{
+    public class CheckVinController : ApiController
+    {
+        // GET api/checkvin/id
+        public string Get(string id)
+        {
+            if (id.Length == 17) return "Ok!!";
+            else return "Not ok!";
+        }
+    }
+}
