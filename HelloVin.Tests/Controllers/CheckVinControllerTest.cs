a70ea5e9923ea9ceb0fc1c16e6f16e740c0956c0
tiedemann <sussi@workport.se>
2015-06-10 10:57:35 +0200

    Created CheckVin controller

---
+++ HelloVin.Tests/Controllers/CheckVinControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloVin;
+using HelloVin.Controllers;
+
+namespace HelloVin.Tests.Controllers
+{
+    [TestClass]
+    public class CheckVinControllerTest
+    {
+        [TestMethod]
+        public void GetByVin()
+        {
+            // Arrange
+            CheckVinController controller = new CheckVinController();
+
+            // Act
+            string result = controller.Get("123");
+
+            // Assert
+            Assert.AreEqual("Not ok", result);
+        }
+    }
+}
