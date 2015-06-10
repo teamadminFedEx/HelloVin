a933484d2ca04a060e2f2bc4103c03c7e2b4425f
tiedemann <sussi@workport.se>
2015-06-10 16:23:50 +0200

    Added SOLID principles

---
+++ HelloVin/Models/VinChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HelloVin.Interfaces;
+
+namespace HelloVin.Models
+{
+    public class VinChecker : IVinChecker
+    {
+         private IEnumerable<IVinRule> _vinRules;
+
+         public VinChecker(IEnumerable<IVinRule> vinRules) 
+         { 
+            _vinRules = vinRules; 
+         } 
+ 
+ 
+         public string Check(string vin) 
+         {
+
+            // Check each rule in the rule list
+             bool result = _vinRules.All(x => x.Check(vin));
+
+             if (result) return ("Ok!!");
+             else return ("Not Ok!");
+         } 
+    }
+}
---
+++ HelloVin/Models/VinLengthRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HelloVin.Interfaces;
+
+namespace HelloVin.Models
+{
+    public class VinLengthRule : IVinRule
+    {
+        public VinLengthRule() 
+        { 
+        } 
+
+        public bool Check(string vin)
+        {
+            return vin.Length == 17;
+        }
+ 
+    }
+}
