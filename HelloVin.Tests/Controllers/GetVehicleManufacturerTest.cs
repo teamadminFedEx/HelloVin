1dfba1849ecb4cd06edc6b46b062c9f069335cfd
unknown <jmom2s@RD0037380.global.scd.scania.com>
2015-06-11 15:50:11 +0200

    handling of both upper and lower case

--- HelloVin.Tests/Controllers/GetVehicleManufacturerTest.cs
+++ HelloVin.Tests/Controllers/GetVehicleManufacturerTest.cs
@@ -15,12 +15,24 @@ namespace HelloVin.Tests.Controllers
             _controller = new VehicleManufacturerController();
         }
         [Test]
-        public void ShouldRecognizeSwedishScaniaVin()
+        public void ShouldRecognizeSwedishScaniaVinInUpperCase()
         {
             //Arrange
             var testVin = "YS2"+"123456789ABCDE";
             
             
+            //Act
+            var resultingCompany = _controller.Get(testVin);
+            //Assert
+            Assert.AreEqual(VehicleManufacturer.Scania, resultingCompany);
+        }
+        [Test]
+        public void ShouldRecognizeSwedishScaniaVinInLoweCase()
+        {
+            //Arrange
+            var testVin = "ys2" + "123456789ABCDE";
+
+
             //Act
             var resultingCompany = _controller.Get(testVin);
             //Assert
