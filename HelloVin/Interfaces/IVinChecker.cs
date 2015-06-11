bc5e5ace66fcff182fb6ad560a8b72b0beb3fa28
hartmark <markus.hartung@r2m.se>
2015-06-11 16:43:03 +0200

    Removed nugget creation from Test

--- HelloVin/Interfaces/IVinChecker.cs
+++ HelloVin/Interfaces/IVinChecker.cs
@@ -1,9 +1,4 @@
 ﻿using HelloVin.Models.DataContract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloVin.Interfaces
 {
