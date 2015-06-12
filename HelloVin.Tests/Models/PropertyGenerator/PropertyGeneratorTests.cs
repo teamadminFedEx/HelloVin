7d8cac141cddb250147d8dc7a2c14419c003969a
unknown <jmom2s@RD0037380.global.scd.scania.com>
2015-06-12 12:40:50 +0200

    added support for all scaniavins in manufacturerpropertygenerator

--- HelloVin.Tests/Models/PropertyGenerator/PropertyGeneratorTests.cs
+++ HelloVin.Tests/Models/PropertyGenerator/PropertyGeneratorTests.cs
@@ -6,27 +6,6 @@ namespace HelloVin.Tests.Models.PropertyGenerator
     [TestFixture]
     public class PropertyGeneratorTests
     {
-        [Test]
-        public void ManufacturerProperty_DetectsScania([Values("YS2","YS4","VLU","9BS")] string id)
-        {
-            var testVin = id + "123456789ABCDE";
-            var manufacturerproperty = new ManufacturerProperty();
-            var result = manufacturerproperty.GetVehicleProperty(testVin);
-
-            Assert.AreEqual(result.Property, "Manufacturer");
-            Assert.AreEqual(result.Value, "Scania");
-        }
-
-        [Test]
-        public void ManufacturerProperty_DefaultsUnknown()
-        {
-            var testVin = "ÄÄÄ" + "123456789ABCDE";
-            var manufacturerproperty = new ManufacturerProperty();
-            var result = manufacturerproperty.GetVehicleProperty(testVin);
-
-            Assert.AreEqual(result.Property, "Manufacturer");
-            Assert.AreEqual(result.Value, "Unknown");
-        }
 
         [Test]
         public void VehicleTypeProperty_DefaultsUnknown()
--- HelloVin/Models/PropertyGenerator/ManufacturerProperty.cs
+++ HelloVin/Models/PropertyGenerator/ManufacturerProperty.cs
@@ -7,7 +7,7 @@ namespace HelloVin.Models.Properties
 {
     public class ManufacturerProperty : IVehiclePropertyGenerator
     {
-        public List<string> Scania = new List<string>
+        public List<string> ScaniaVin = new List<string>
         {
             "YS2",
             "9BS",
@@ -22,7 +22,7 @@ namespace HelloVin.Models.Properties
             var value = VehicleManufacturer.Unknown;
 
             var toCheck = Vin.Substring(0, 3).ToUpper();
-            if (Scania.Contains(toCheck))
+            if (ScaniaVin.Contains(toCheck))
             {
                 value = VehicleManufacturer.Scania;
             }
